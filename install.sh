2b71f7857be239c6be7089a8a96cedbf14bcf827
Stephan <stekoe@outlook.com>
2025-03-02 19:51:19 +0800

    Move extensions to extensions/ folder

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ ext_name="${1:?}"
 [ ! -e "${INSTALL_DIR}" ] && mkdir -p "${INSTALL_DIR}"
 cat "${SCRIPT_PATH}/utils"/*.py \
     <(echo "_T = \\") \
-    <(python3 "${SCRIPT_PATH}/tools/convert_yaml_to_json.py" "${SCRIPT_PATH}/${ext_name}/strings.yaml") \
-    <(sed "/from utils/d;/import utils/d;/from ${ext_name}/d;/import ${ext_name}/d" "${SCRIPT_PATH}/${ext_name}"/*.py) \
+    <(python3 "${SCRIPT_PATH}/tools/convert_yaml_to_json.py" "${SCRIPT_PATH}/extensions/${ext_name}/strings.yaml") \
+    <(sed "/from utils/d;/import utils/d;/from extensions.${ext_name}/d;/import extensions.${ext_name}/d;/import \./d;/import \.\./d" "${SCRIPT_PATH}/extensions/${ext_name}"/*.py) \
     > "${INSTALL_DIR}/${ext_name}.py"
 
 echo "The Nautilus file manager must be restarted before this extension can be used. Restart Nautilus now? [y/N]"
